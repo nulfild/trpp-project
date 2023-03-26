20f0acea200076ac9a59378a32d35c72b2beee6d
German Kunin <kunin.german@icloud.com>
2023-03-26 20:43:47 +0300

    Добавил несколько моделей для "мозга" игры

---
+++ TRPP/Model/Colors.swift
@@ -0,0 +1,39 @@
+//
+//  Colors.swift
+//  TRPP
+//
+//  Created by Герман Кунин on 26.03.2023.
+//
+
+import UIKit
+
+struct Colors {
+   
+    let borderButtonColor = UIColor(red: 0.8, green: 0.8, blue: 0.8, alpha: 1).cgColor
+  
+    let blueGr: [CGColor] = [
+       UIColor(red: 0.26, green: 0.46, blue: 0.55, alpha: 1).cgColor,
+       UIColor(red: 0.125, green: 0.225, blue: 0.375, alpha: 1).cgColor,
+       UIColor(red: 0.26, green: 0.46, blue: 0.55, alpha: 1).cgColor,
+    ]
+
+    let redGr: [CGColor] = [
+        UIColor(red: 0.78, green: 0.52, blue: 0.42, alpha: 1).cgColor,
+        UIColor(red: 0.58, green: 0.073, blue: 0.0, alpha: 1).cgColor,
+        UIColor(red: 0.78, green: 0.52, blue: 0.42, alpha: 1).cgColor,
+    ]
+    
+    let greenGr: [CGColor] = [
+        UIColor(red: 0.37, green: 0.64, blue: 0.38, alpha: 1).cgColor,
+        UIColor(red: 0.1, green: 0.31, blue: 0.11, alpha: 1).cgColor,
+        UIColor(red: 0.37, green: 0.64, blue: 0.38, alpha: 1).cgColor,
+    ]
+    
+    let yellowGr: [CGColor] = [
+        UIColor(red: 0.78, green: 0.7, blue: 0.431, alpha: 1).cgColor,
+        UIColor(red: 0.558, green: 0.436, blue: 0.004, alpha: 1).cgColor,
+        UIColor(red: 0.78, green: 0.7, blue: 0.431, alpha: 1).cgColor,
+
+    ]
+}
+
